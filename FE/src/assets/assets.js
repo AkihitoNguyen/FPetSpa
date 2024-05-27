9febb944ae68a6f033745f73671b137c71a99bfa
for-everyoung12 <sonthse172913@fpt.edu.vn>
2024-05-27 21:19:48 +0700

    update code for frontend

---
+++ FE/src/assets/assets.js
@@ -0,0 +1,70 @@
+import cat_1 from './cat_1.png'
+import cat_2 from './cat_2.png'
+import cat_3 from './cat_3.png'
+
+import cat_spa from './cat_spa.png'
+import cat from './cat.png'
+
+
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import nav_bg from './nav_bg.png'
+import logo from './logo.png'
+import cart from './cart.png'
+import left_arrow from './left_arrow.png'
+import right_arrow from './right_arrow.png'
+
+import pet_group from './pet_group.png'
+import pet_group2 from './pet_group2.png'
+
+import search from './search.png'
+import phone from './phone.png'
+import icon_1 from './icon_1.png'
+import icon_2 from './icon_2.png'
+import icon_3 from './icon_3.png'
+import facebook from './facebook.png'
+import instagram from './instagram.png'
+import email from './email.png'
+
+export const assets = {
+    logo,
+    cart,
+    cat_1,
+    cat_2,
+    cat_3,
+    cat_spa,
+    cat,
+    food_1,
+    food_2,
+    food_3,
+    food_4,
+    food_5,
+    left_arrow,
+    right_arrow,
+    pet_group,
+    pet_group2,
+    search,
+    nav_bg,
+    phone,
+    icon_1,
+    icon_2,
+    icon_3,
+    facebook,
+    instagram,
+    email
+}
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "Greek salad",
+        image: food_1,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Salad"
+    },
+]
+
---
+++ FE/src/pages/Cart/Cart.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react'
+import './Cart.css'
+const Cart = () => {
+
+  const {cartItems,product_list, removeFromCart} = useContext(Store)
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Cart
