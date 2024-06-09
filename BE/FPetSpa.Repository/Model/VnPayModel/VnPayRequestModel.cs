c2ae8d7b3ada2cb321cea6f02ab451cfa589d078
Hùng Nguyễn <hungnpse172907@fpt.edu.vn>
2024-06-09 18:16:52 +0700

    Update VNPay function

--- BE/FPetSpa.Repository/Model/VnPayModel/VnPayRequestModel.cs
+++ BE/FPetSpa.Repository/Model/VnPayModel/VnPayRequestModel.cs
@@ -6,6 +6,7 @@
         public string Description { get; set; }
         public double Amount {  get; set; }
         public DateTime CreatedDate { get; set; }
+        public DateTime ExpiredDate { get; set; }
         public string OrderId { get; set; }
     }
 }
--- BE/FPetSpa.Repository/Services/VnPay/IVnPayService.cs
+++ BE/FPetSpa.Repository/Services/VnPay/IVnPayService.cs
@@ -9,7 +9,10 @@ namespace FPetSpa.Repository.Model.VnPayModel
 {
     public interface IVnPayService
     {
-        string CreatePaymentURl(HttpContext httpContent, VnPayRequestModel);
+        string CreatePaymentURl(VnPayRequestModel vnPayRequestModel, HttpContext context);
         VnPayResponseModel PayymentExecute(IQueryCollection collection);
+
+
+
     }
 }
