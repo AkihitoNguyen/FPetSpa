7aadf4e171319b15cd706521205533c9167a7c42
TuanAnh-LV <anhlvtse172914@fpt.edu.vn>
2024-07-14 14:27:14 +0700

    update contact us, feedback

--- FE/src/components/Footer/Footer.jsx
+++ FE/src/components/Footer/Footer.jsx
@@ -1,66 +1,48 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import { assets } from '../../assets/assets'
+
 const Footer = () => {
-
     return (
-        <div className='flex items-center flex-col text-white'>
-            <img src={assets.logo} alt="" className='block w-[124px] h-[124px] mt-14 mb-10'/>
-            <h2 className='text-[54px] block leading-[64.8px] mb-6 font-bold'>Level up your inbox</h2>
+        <div className='flex flex-col items-center text-white bg-gray-900 py-14'>
+            <img src={assets.logo} alt="Logo" className='w-32 h-32 mt-4 mb-6'/>
+            <h2 className='text-4xl font-bold mb-4'>Level up your inbox</h2>
 
-            <div className='flex gap-4 mb-10'>
-                <div>
-                    <input type="text" placeholder='Email address*' className=' text-black cursor-text block text-[16px] px-[24px] py-[8px]  font-normal rounded-[2rem] leading-[1.8px]'/>
-                </div>
-                <div>
-                    <button className='border-[1px] rounded-[2rem] bg-white text-[#000000] px-[24px] py-[11.2px] cursor-pointer text-[14px] font-semibold'>Sign Up</button>
-                </div>
+            <div className='flex gap-4 mb-8'>
+                <input 
+                    type="text" 
+                    placeholder='Email address*' 
+                    className='text-black text-lg px-6 py-3 rounded-full w-64'
+                />
+                <button className='bg-white text-black px-6 py-3 rounded-full font-semibold'>Sign Up</button>
             </div>
 
-            <div className='flex items-start gap-80 text-start'>
-                <div className='flex items-start flex-col bg-cover text-left relative'>
-                    <h6 className='pl-8'>Support</h6>
-                    <ul className=''>
-                        <li className='text-start'>
-                            <a href="">FAQs</a>
-                        </li>
-                        <li>
-                            <a href="">Quality Assurance</a>
-                        </li>
-                        <li>
-                            <a href="">Return Policy</a>
-                        </li>
-                        <li>
-                            <a href="">Privacy Policy</a>
-                        </li>
+            <div className='flex gap-20 text-left text-sm'>
+                <div className='flex flex-col'>
+                    <h6 className='text-lg mb-2'>Support</h6>
+                    <ul className='space-y-2'>
+                        <li><a href="" className='hover:underline'>FAQs</a></li>
+                        <li><a href="" className='hover:underline'>Quality Assurance</a></li>
+                        <li><a href="" className='hover:underline'>Return Policy</a></li>
+                        <li><a href="" className='hover:underline'>Privacy Policy</a></li>
                     </ul>
                 </div>
-                <div>
-                    <h6 className='pl-8'>Fpet Spa</h6>
-                    <ul>
-                        <li>
-                            <a href="">About Fpet</a>
-                        </li>
-                        <li>
-                            <a href="">Find a Store</a>
-                        </li>
+                <div className='flex flex-col'>
+                    <h6 className='text-lg mb-2'>Fpet Spa</h6>
+                    <ul className='space-y-2'>
+                        <li><a href="" className='hover:underline'>About Fpet</a></li>
+                        <li><a href="" className='hover:underline'>Find a Store</a></li>
                     </ul>
                 </div>
-                <div>
-                    <h6 className='pl-8'>Connect</h6>
-                    <ul>
-                        <li>
-                            <a href="">Contact Us</a>
-                        </li>
-                        <li>
-                            <a href="">fpet@gmail.com</a>
-                        </li>
-                        <li>
-                            <div className='w-10 flex gap-4'>
-                                <img src={assets.facebook} alt="" className='' />
-                                <img src={assets.instagram} alt="" className='' />
-                                <img src={assets.email} alt="" className=''/>
-                            </div>
+                <div className='flex flex-col'>
+                    <h6 className='text-lg mb-2'>Connect</h6>
+                    <ul className='space-y-2'>
+                        <li><a href="" className='hover:underline'>Contact Us</a></li>
+                        <li><a href="mailto:fpet@gmail.com" className='hover:underline'>fpet@gmail.com</a></li>
+                        <li className='flex gap-4'>
+                            <a href=""><img src={assets.facebook} alt="Facebook" className='w-6 h-6'/></a>
+                            <a href=""><img src={assets.instagram} alt="Instagram" className='w-6 h-6'/></a>
+                            <a href=""><img src={assets.email} alt="Email" className='w-6 h-6'/></a>
                         </li>
                     </ul>
                 </div>
