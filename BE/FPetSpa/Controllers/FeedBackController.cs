183f2a8ae15498ae5021a45b1428f153d69e2a4b
Ivypearl34107 <ninhphse172470@fpt.edu.vn>
2024-07-15 05:33:47 +0700

    Update Error Feedback

--- BE/FPetSpa/Controllers/FeedBackController.cs
+++ BE/FPetSpa/Controllers/FeedBackController.cs
@@ -14,150 +14,108 @@ namespace FPetSpa.Controllers
     public class FeedBackController : ControllerBase
     {
         private readonly UnitOfWork _unitOfWork;
-        public FeedBackController(UnitOfWork unitOfWork)
+        private readonly IFeedBackService _feedBackService;
+        public FeedBackController(IFeedBackService feedBackService)
         {
+
             _feedBackService = feedBackService;
         }
-        /// <summary>
-        /// SortBy (UserFeedBackId = 1,Star = 2)
-        /// 
-        /// SortType (Ascending = 1,Descending = 2,)        
-        /// </summary>
-        /// <param name="RequestSearchFeedBackModel"></param>
-        /// <returns></returns>
-        [HttpGet("SearchFeedback")]
-        public async Task<IActionResult> SearchFeedBack([FromQuery] RequestSearchFeedBackModel requestSearchFeedbackModel)
-        {
-            var sortBy = requestSearchFeedbackModel.SortContent != null ? requestSearchFeedbackModel.SortContent?.sortFeedBackBy.ToString() : null;
-            var sortType = requestSearchFeedbackModel.SortContent != null ? requestSearchFeedbackModel.SortContent?.sortFeedBackType.ToString() : null;
-            Expression<Func<FeedBack, bool>> filter = x =>
-                (string.IsNullOrEmpty(requestSearchFeedbackModel.UserFeedBackId) || x.UserFeedBackId.Contains(requestSearchFeedbackModel.UserFeedBackId)) &&
-                x.Star >= requestSearchFeedbackModel.FromStar &&
-                (x.Star <= requestSearchFeedbackModel.ToStar || requestSearchFeedbackModel.ToStar == null);
-            Func<IQueryable<FeedBack>, IOrderedQueryable<FeedBack>> orderBy = null;
-
-            if (!string.IsNullOrEmpty(sortBy))
+            [HttpGet]
+            public async Task<ActionResult<List<FeedBack>>> GetAllFeedBack()
             {
-                if (sortType == SortFeedbackTypeEnum.Ascending.ToString())
+                try
                 {
-                    orderBy = query => query.OrderBy(p => EF.Property<object>(p, sortBy));
+                    var feedbacks = await _feedBackService.GetAllFeedBack();
+                    return Ok(feedbacks);
                 }
-                else if (sortType == SortFeedbackTypeEnum.Descending.ToString())
+                catch (Exception ex)
                 {
-                    orderBy = query => query.OrderByDescending(p => EF.Property<object>(p, sortBy));
+                    return StatusCode(500, $"Internal server error: {ex.Message}");
                 }
             }
-            var responseCategorie = _unitOfWork.FeedBackRepository.Get(
-                filter,
-                orderBy,
-                includeProperties: "",
-                pageIndex: requestSearchFeedbackModel.pageIndex,
-                pageSize: requestSearchFeedbackModel.pageSize
-            );
-
-
-            return Ok(responseCategorie);
-        }
-        [HttpGet("GetAllFeedBack")]
-        public async Task<IActionResult> GetAll()
-        {
-            var result = await _unitOfWork.FeedBackRepository.GetAll();
-            return Ok(result);
-        [HttpGet]
-        public async Task<ActionResult<List<FeedBack>>> GetAllFeedBack()
-        {
-            try
+            [HttpGet("id")]
+            public async Task<ActionResult<FeedBack>> GetFeedBackById(int id)
             {
-                var feedbacks = await _feedBackService.GetAllFeedBack();
-                return Ok(feedbacks);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Internal server error: {ex.Message}");
-            }
-        }
-        [HttpGet("id")]
-        public async Task<ActionResult<FeedBack>> GetFeedBackById(int id)
-        {
-            try
-            {
-                var feedback = await _feedBackService.GetFeedBackById(id);
-                if (feedback == null)
+                try
                 {
-                    return NotFound($"Feedback with id {id} not found");
+                    var feedback = await _feedBackService.GetFeedBackById(id);
+                    if (feedback == null)
+                    {
+                        return NotFound($"Feedback with id {id} not found");
+                    }
+                    return Ok(feedback);
                 }
-                return Ok(feedback);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Internal server error: {ex.Message}");
-            }
-        }
-
-        [HttpGet("productId")]
-        public async Task<ActionResult<FeedBack>> GetFeedBackByProductId(string productId)
-        {
-            try
-            {
-                var feedback = await _feedBackService.GetFeedBackByProductId(productId);
-                if (feedback == null)
+                catch (Exception ex)
                 {
-                    return NotFound($"Feedback with id {productId} not found");
+                    return StatusCode(500, $"Internal server error: {ex.Message}");
                 }
-                return Ok(feedback);
             }
-            catch (Exception ex)
+
+            [HttpGet("productId")]
+            public async Task<ActionResult<FeedBack>> GetFeedBackByProductId(string productId)
             {
-                return StatusCode(500, $"Internal server error: {ex.Message}");
+                try
+                {
+                    var feedback = await _feedBackService.GetFeedBackByProductId(productId);
+                    if (feedback == null)
+                    {
+                        return NotFound($"Feedback with id {productId} not found");
+                    }
+                    return Ok(feedback);
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, $"Internal server error: {ex.Message}");
+                }
             }
+
+            [HttpPost("Create")]
+            public async Task<ActionResult> CreateFeedBack([FromBody] RequestFeedBackModel feedBack)
+            {
+                try
+                {
+                    await _feedBackService.CreateFeedBack(feedBack);
+                    return Ok();
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, $"Internal server error: {ex.Message}");
+                }
+            }
+
+            [HttpPut("Update")]
+            public async Task<ActionResult> UpdateFeedBack(int id, [FromBody] RequestFeedBackModel feedBack)
+            {
+                try
+                {
+
+                    await _feedBackService.UpdateFeedBack(id, feedBack);
+                    return Ok();
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, $"Internal server error: {ex.Message}");
+                }
+            }
+
+            [HttpDelete("{id}")]
+            public IActionResult DeleteFeedBack(int id)
+            {
+                try
+                {
+                    _feedBackService.DeleteFeedBack(id);
+                    return Ok();
+                }
+                catch (ApplicationException ex)
+                {
+                    return BadRequest(new { message = ex.Message });
+                }
+            }
+
+
+
+
         }
-
-        [HttpPost("Create")]
-        public async Task<ActionResult> CreateFeedBack([FromBody] RequestFeedBackModel feedBack)
-        {
-            try
-            {
-                await _feedBackService.CreateFeedBack(feedBack);
-                return Ok();
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Internal server error: {ex.Message}");
-            }
-        }
-
-        [HttpPut("Update")]
-        public async Task<ActionResult> UpdateFeedBack(int id, [FromBody] RequestFeedBackModel feedBack)
-        {
-            try
-            {
-
-                await _feedBackService.UpdateFeedBack(id, feedBack);
-                return Ok();
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Internal server error: {ex.Message}");
-            }
-        }
-
-        [HttpDelete("{id}")]
-        public IActionResult DeleteFeedBack(int id)
-        {
-            try
-            {
-                _feedBackService.DeleteFeedBack(id);
-                return Ok();
-            }
-            catch (ApplicationException ex)
-            {
-                return BadRequest(new { message = ex.Message });
-            }
-        }
-
-
-
 
     }
 
-}
